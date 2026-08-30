8a0a0e317044127c76ca4b45e637937ab952bca5
Ivan Bodnarash <bodnarash1999@gmail.com>
2026-08-30 19:14:58 +0200

    feat: services type, new schema for sanity

--- studio-kazkovyi-dim/schemaTypes/index.ts
+++ studio-kazkovyi-dim/schemaTypes/index.ts
@@ -3,6 +3,5 @@ import { aboutUsType } from './aboutUsType'
 import { charsType } from './charsType'
 import { reviewsType } from './reviewsType'
 import { servicesType } from './servicesType'
-import { servicesItems } from './serviceItems'
 
-export const schemaTypes = [eventsType, aboutUsType, charsType, reviewsType, servicesType, servicesItems]
+export const schemaTypes = [eventsType, aboutUsType, charsType, reviewsType, servicesType]
